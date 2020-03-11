0b221a093da50511fb383f5c5ce3483d4a232dfc
EricZimmermann <eric.zimmermann@mail.mcgill.ca>
2020-03-11 16:01:54 -0400

    more bug fixes

--- cortex/imaging/kernel.c
+++ cortex/imaging/kernel.c
@@ -11,7 +11,7 @@ struct Kernel{
 	float **data;
 }
 
-void initKernel(struct Kernel, uint8_t size){
+Kernel initKernel(struct Kernel, uint8_t size){
 	Kernel.size = size;
 	Kernel.data = malloc(size * sizeof(float *));
 	for(uint8_t row = 0; row < size; i++){
@@ -29,16 +29,16 @@ void deinitKernel(struct Kernel){
 
 void generateFilter(struct Kernel, sigma){    
 	float sum = 0;
-	int half_band = Kernel.size / 2;
-    for (i = - half_band; i <= half_band; i++) {
-        for (j = - half_band; j <= half_band; j++) {
+	uint8_t half_band = Kernel.size / 2;
+    for (uint8_t i = - half_band; i <= half_band; i++) {
+        for (uint8_t j = - half_band; j <= half_band; j++) {
             Kernel.data[i+half_band][j+half_band]= exp(-1*((pow(i, 2) + pow(j, 2)) / ((2 * pow(sigma, 2))))); // unnormalized guassian
             sum += Kernel.data[i+half_band][j+half_band] // normalization pooling
         }
     }
     
-    for (i = - half_band; i <= half_band; i++) {
-        for (j = -half_band; j < half_band; j++) {
+    for (uint8_t i = - half_band; i <= half_band; i++) {
+        for (uint8_t j = -half_band; j < half_band; j++) {
         	Kernel.data[i+half_band][j+half_band] /= sum;
         }
     }

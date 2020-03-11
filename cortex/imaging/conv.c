92b5ff88921998e9dbc56d070294feb8edfda94e
EricZimmermann <eric.zimmermann@mail.mcgill.ca>
2020-03-11 15:34:57 -0400

    simulated padded borders added

--- cortex/imaging/conv.c
+++ cortex/imaging/conv.c
@@ -8,18 +8,35 @@ Convolutional operations
 
 void convolve(struct Image, struct Kernel){
 	uint8_t half_band = Kernel.size / 2;
+	
+	// image iterables
 	for(uint8_t irow  = 0, irow < Image.size; ++irow){
 		for(uint8_t icol  = 0, icol < Image.size; ++icol){
-			if(irow > half_band && icol > half_band){
-				for(uint8_t krow = 0; krow < Kernel.size; ++krow){
-					for(uint8_t kcol = 0; kcol < Kernel.size; ++kcol){
-						Image.data[irow][icol] += Kernel.data[krow][kcol] * Image.data[irow - half_band + krow][icol - half_band + kcol];
+			
+			// kernel iterables
+			for(uint8_t krow = 0; krow < Kernel.size; ++krow){
+				for(uint8_t kcol = 0; kcol < Kernel.size; ++kcol){
+					
+					// image-kernel index mapping
+					uint8_t rindex = irow - half_band + krow;
+					uint8_t cindex = irow - half_band + krow
+					
+					// simulate padded borders
+					if(rindex < 0){
+						rindex = 0;
+					}else if(rindex > Kernel.size -1){
+						rindex = Kernel.size -1;
 					}
-				}
-			}else{
-				// reflection rules here
-			}
+					if(cindex < 0){
+						cindex = 0;
+					}else if(cindex > Kernel.size -1){
+						cindex = Kernel.size -1;
+					}
 
+					// convolve 
+					Image.data[irow][icol] += Kernel.data[krow][kcol] * Image.data[rindex][cindex];
+				}
+			}
 		}
 	}
 }

6d7ade021fdfc7bf2b919140b6feac977f32e653
EricZimmermann <eric.zimmermann@mail.mcgill.ca>
2020-04-22 12:24:47 -0400

    preprocessing script validated

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -6,20 +6,20 @@ from copy import deepcopy
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', type=str, required=True)
 parser.add_argument('--output', type=str, required=True)
-parser.add_argument('--size', type=str, required=True)
+parser.add_argument('--size', type=int, required=True)
 parser.add_argument('--p-noise', type=float, required=False, default=0.3)
 parser.add_argument('--r-noise', type=float, required=False, default=5)
 
 def main():
     
     args = parser.parse_args()
-    input_img = utils.loadImage(args.input)
+    input_img = utils.loadImage(args.input, args.size)
     noisey_img = deepcopy(input_img)
     utils.fuzz(noisey_img, args.r_noise, args.p_noise)
     txt_img = utils.listify(input_img)
     txt_noise = utils.listify(input_img)
-    utils.saveTxt(txt_img, os.path.join(output, 'clean.txt'))
-    utils.saveTxt(txt_noise, os.path.join(output, 'noise.txt'))
+    utils.saveTxt(txt_img, os.path.join(args.output, 'clean.txt'))
+    utils.saveTxt(txt_noise, os.path.join(args.output, 'noise.txt'))
     
 if __name__ == '__main__':
     main()
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,23 +1,27 @@
 import cv2
 import numpy
 
-def loadImage(filepath):
-	return cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+def loadImage(filepath, size):
+    img =  cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    return cv2.resize(img, (size, size))
 
 def saveTxt(image, filepath):
-	image_stream = listify(image)
-	with open(filepath, "w") as output:
-    	output.write(str(image))
+    image_stream = listify(image)
+    with open(filepath, "w") as output:
+        for px in image_stream[:-1]:
+            output.write(str(px)+',')
+        output.write(str(image_stream[-1]))
 
 def listify(image):
- 	return image.flatten().tolist()
+    flat = image.flatten()
+    return flat
 
 def fuzz(image, ranges, p=0.15):
-	rows, cols = image.shape
-	for row in range(rows):
-		for col in range(cols):
-			if(numpy.random.uniform() < p):
-				image[row][col] += numpy.random.randint(-ranges, ranges)
+    rows, cols = image.shape
+    for row in range(rows):
+        for col in range(cols):
+            if(numpy.random.uniform() < p):
+                image[row][col] += numpy.random.randint(-ranges, ranges)
 
 def psnr(img_one, img_two):
 	return cv2.PSNR(img1, img2)
